d7eccbcf0ffcb01623474ffdfa1b16f3fafa815a
Kirill Penkin <lambda.function.486@gmail.com>
2026-06-17 02:29:55 +0400

    Fix GitHub Pages icon path

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Arty, Impacts, Officer, Target } from './hoc/items';
 import { MeasurementModal } from './hoc/modal';
 
 function App() {
+  const publicUrl = process.env.PUBLIC_URL;
+
   return (
     <div className="App">
       <h1 className="app-title">
-        <img className="app-title-icon" src="/artillery-icon.svg" alt="" />
+        <img className="app-title-icon" src={`${publicUrl}/artillery-icon.svg`} alt="" />
         <span>Foxhole artillery calculator</span>
       </h1>
       <Arty />
